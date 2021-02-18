967a563ea87b7be1f47b7438e9d4369a182c3045
wiweka24 <yogomu24@gmail.com>
2021-02-18 08:56:02 +0700

    menambahkan template c# console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PBO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
