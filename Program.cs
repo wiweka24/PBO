71636b7d88a917f93b494bf9bfb9e57515c17d23
wiweka24 <yogomu24@gmail.com>
2021-02-18 10:22:17 +0700

    edit Program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace PBO
+namespace oopfundamental
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Masukkan Merk Kendaraanmu?");
+            string merk = Console.ReadLine();
+            Console.WriteLine("Masukkan Model Kendaraan?");
+            string model = Console.ReadLine();
+            Console.WriteLine("Masukkan Horse Power Kendaraan Anda");
+            double hp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Apakah kendaraan Anda menggunakan Turbo Charger? (0/1)");
+           
+            double kecepatan = 0;
+            kecepatan = (hp / 4)* 1.5 + 100;
+           
+            Console.WriteLine("Estimasi Kecepatan Kendaraan?");
+            
+            Console.WriteLine(String.Format("Estimasi Kecepatan Optimal adalah {0} KM/H",kecepatan));
+            
+            
+
+
+            
+
         }
     }
 }
